c2f9eb2b4a8f6ced2f7b16750ab9fef22f81ee02
mforazure9@gmail.com <mforazure9@gmail.com>
2024-03-06 17:38:41 +0530

    OOPS

---
+++ OOPS/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace OOPS
+{
+    public class Person
+    {
+        public string Name;
+        public void interoduce(string to)
+        {
+            Console.WriteLine( "hi {0}, I am {1}", to, Name);
+        }
+    }
+
+}
---
+++ OOPS/program.cs
@@ -0,0 +1,14 @@
+﻿namespace OOPS
+{
+    class  Program
+     {
+        static void Main(string[] arg)
+        { 
+            var  person= new Person();
+
+            person.Name = "sudarshan";
+            person.interoduce("shetty");
+        }
+    }
+
+}
